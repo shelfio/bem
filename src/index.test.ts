1ef8a3010cf8569ed8b0e271a0e56cdd96c87ab1
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2023-06-16 19:08:21 +0300

    refactor!: reuse classMap feature of bem-cn instead of custom solution
    BREAKING CHANGE: remove b & blazeBlock support. Use the return function of createBlock

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,168 +1,100 @@
-import {b, blazeHelper, createBlock} from './index';
+import {createBlock} from './index';
 
-describe('bem lib functions', () => {
-  it('block should not work with null and undefined', () => {
-    try {
-      createBlock(undefined).toString();
-    } catch (error) {
-      expect(error.toString()).toBe('Error: Block name should be non-empty');
-    }
-    try {
-      createBlock(null).toString();
-    } catch (error) {
-      expect(error.toString()).toBe('Error: Block name should be non-empty');
-    }
-  });
+it('should not work with null and undefined', () => {
+  try {
+    // @ts-expect-error
+    createBlock(undefined);
+  } catch (error: any) {
+    expect(error.toString()).toBe('Error: Block name should be a string');
+  }
+  try {
+    // @ts-expect-error
+    createBlock(null);
+  } catch (error: any) {
+    expect(error.toString()).toBe('Error: Block name should be a string');
+  }
+});
 
-  it('block should return string for class', () => {
-    expect(createBlock('Warthog').toString()).toBe('Warthog');
-  });
+it('should return bem object', () => {
+  const block = createBlock('Warthog');
+  expect(block).toBeInstanceOf(Function);
+  expect(block.toString()).toBe('Warthog');
+});
 
-  it('block to be class of BEM', () => {
-    const block = createBlock('Hedgehog');
+it('return classname when no modifiers provided', () => {
+  const block = createBlock('Hedgehog');
 
-    expect(block()).toBe('Hedgehog');
-  });
+  expect(block().toString()).toBe('Hedgehog');
+});
 
-  it('block to be class of BEM without element, but with modifier and original class', () => {
-    const block = createBlock('Badger');
+it('should return class name', () => {
+  expect(createBlock('Warthog').toString()).toBe('Warthog');
+});
 
-    expect(block(null, {['']: 'modifier'}).toString()).toBe('Badger Badger--modifier');
-  });
+it('should return classname with element', () => {
+  const block = createBlock('Drake');
 
-  it('block to be class of BEM with element', () => {
-    const block = createBlock('Drake');
+  expect(block('element').toString()).toBe('Drake__element');
+});
 
-    expect(block('element').toString()).toBe('Drake__element');
-  });
+it('should return classname with modifiers only', () => {
+  const block = createBlock('Badger');
 
-  it('block to be class of BEM with element and modifier', () => {
-    const block = createBlock('Eft');
+  expect(block({['']: 'modifier'}).toString()).toBe('Badger Badger--modifier');
+  expect(block({modifier: true}).toString()).toBe('Badger Badger--modifier');
+});
 
-    expect(block('element', {['']: 'modifier'}).toString()).toBe(
-      'Eft__element Eft__element--modifier'
-    );
-  });
+it('should return classname with element and modifiers', () => {
+  const block = createBlock('Eft');
 
-  it('block to be class of BEM with cssModule', () => {
-    const cssModule = {
-      Hedgehog: 'Hedgehog_index',
-    };
+  expect(block('element', {modifier: true}).toString()).toBe('Eft__element Eft__element--modifier');
+});
 
-    const block = createBlock('Hedgehog', cssModule);
+it('should return block class with cssModule', () => {
+  const cssModule = {
+    Hedgehog: 'Hedgehog_index',
+  };
 
-    expect(block()).toBe('Hedgehog_index');
-  });
+  const block = createBlock('Hedgehog', cssModule);
 
-  it('block to be class of BEM without element, but with modifier and original class with cssModule', () => {
-    const cssModule = {
-      Badger: 'Badger_index',
-      'Badger--modifier': 'Badger--modifier_index2',
-    };
+  expect(block()).toBe('Hedgehog_index');
+});
 
-    const block = createBlock('Badger', cssModule);
+it('should return class of BEM with element with cssModule', () => {
+  const cssModule = {
+    Drake__element: 'Drake__element_id2',
+  };
 
-    expect(block(null, {['']: 'modifier'})).toBe('Badger_index Badger--modifier_index2');
-  });
+  const block = createBlock('Drake', cssModule);
 
-  it('block to be class of BEM with element with cssModule', () => {
-    const cssModule = {
-      Drake__element: 'Drake__element_id2',
-    };
+  expect(block('element').toString()).toBe('Drake__element_id2');
+});
 
-    const block = createBlock('Drake', cssModule);
+it('should return class of BEM with element with modifier and with cssModule', () => {
+  const cssModule = {
+    Eft__element: 'Eft__element_id',
+    'Eft__element--modifier': 'Eft__element--modifier_id2',
+  };
 
-    expect(block('element')).toBe('Drake__element_id2');
-  });
+  const block = createBlock('Eft', cssModule);
 
-  it('block to be class of BEM with element with modifier and with cssModule', () => {
-    const cssModule = {
-      Eft__element: 'Eft__element_id',
-      'Eft__element--modifier': 'Eft__element--modifier_id2',
-    };
+  expect(block('element', {modifier: true}).toString()).toBe(
+    'Eft__element_id Eft__element--modifier_id2'
+  );
+});
 
-    const block = createBlock('Eft', cssModule);
+it('should return classname without id if not match in object', () => {
+  const cssModule = {
+    some: 'some_id',
+  };
 
-    expect(block('element', {['']: 'modifier'})).toBe('Eft__element_id Eft__element--modifier_id2');
-  });
+  const block = createBlock('Eft', cssModule);
 
-  it('block should return classname without id if not match in object', () => {
-    const cssModule = {
-      some: 'some_id',
-    };
+  expect(block()).toBe('Eft');
+});
 
-    const block = createBlock('Eft', cssModule);
-
-    expect(block().toString()).toBe('Eft');
-  });
-
-  it('blazeHelper to be class of BEM', () => {
-    const block = createBlock('Fawn');
-
-    expect(blazeHelper(block).toString()).toBe('Fawn');
-  });
-
-  it('blazeHelper to be class of BEM without element, but with modifier and original class', () => {
-    const block = createBlock('Gibbon');
-
-    expect(blazeHelper(block, '', 'modifier').toString()).toBe('Gibbon Gibbon--modifier');
-  });
-
-  it('blazeHelper to be class of BEM without element, but with modifier as array', () => {
-    const block = createBlock('Gibbon');
-
-    expect(blazeHelper(block, '', ['modifier']).toString()).toBe('Gibbon Gibbon--modifier');
-  });
-
-  it('blazeHelper to be class of BEM with element', () => {
-    const block = createBlock('Heron');
-
-    expect(blazeHelper(block, 'element').toString()).toBe('Heron__element');
-  });
-
-  it('blazeHelper to be class of BEM with element and modifier', () => {
-    const block = createBlock('Jackalope');
-
-    expect(blazeHelper(block, 'element', 'modifier').toString()).toBe(
-      'Jackalope__element Jackalope__element--modifier'
-    );
-  });
-
-  it('b to be class of BEM', () => {
-    const block = createBlock('Koala');
-
-    expect(b(block)).toBe('Koala');
-  });
-
-  it('b to be class of BEM without element, but with modifier and original class', () => {
-    const block = createBlock('Lynx');
-
-    expect(b(block, '', 'modifier')).toBe('Lynx Lynx--modifier');
-  });
-
-  it('b to be class of BEM with element', () => {
-    const block = createBlock('Meerkat');
-
-    expect(b(block, 'element')).toBe('Meerkat__element');
-  });
-
-  it('b to be class of BEM with element and modifier', () => {
-    const block = createBlock('Narwhal');
-
-    expect(b(block, 'element', 'modifier')).toBe('Narwhal__element Narwhal__element--modifier');
-  });
-
-  it('b to be class of BEM with element and modifier in array', () => {
-    const block = createBlock('Narwhal');
-
-    expect(b(block, 'element', ['modifier'])).toBe('Narwhal__element Narwhal__element--modifier');
-  });
-
-  it('b to be class of BEM with element and multiple modifiers in array', () => {
-    const block = createBlock('Narwhal');
-
-    expect(b(block, 'element', ['modifier', 'second-modifier'])).toBe(
-      'Narwhal__element Narwhal__element--modifier Narwhal__element--second-modifier'
-    );
-  });
+it('should return bem object', () => {
+  const block = createBlock('Warthog');
+  expect(block).toBeInstanceOf(Function);
+  expect(block.toString()).toBe('Warthog');
 });
